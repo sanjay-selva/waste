8cefc52832373743f37ba971fe42d6b7e66a3556
Sanjay <sanjaysanjay51734@gmal.com>
2026-04-18 20:50:08 +0530

    change correct formate

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: './',
-
-
-   server: {
-    port: 5173,
-    open: true
-  },
-
- build: {
-  chunkSizeWarningLimit: 1000
-}
+  base: './'
 })
